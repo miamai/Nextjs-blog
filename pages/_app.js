2747eaf17269ce519943e03e0bed83d91d906052
miamai <cb121466@gmail.com>
2023-02-16 23:58:42 +0800

    add open HomeSplash state

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { ThemeModeProvider } from '../styles/ThemeModeProvider';
 
 export default function App({ Component, pageProps }) {
+  const [openHomeSplash, setOpenHomeSplash] = useState(true);
+
   useEffect(() => {
     AOS.init({
       easing: 'ease',
       duration: 400,
       delay: 150,
     });
+
+    const timer = setTimeout(() => {
+      setOpenHomeSplash(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
+
   return (
     <ThemeModeProvider>
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        open={openHomeSplash}
+        setOpen={setOpenHomeSplash}
+      />
     </ThemeModeProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -13,95 +13,127 @@ import {
   CardMedia,
   Chip,
 } from '@mui/material';
-
-import Layout from '../components/layout';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Layout from '../components/Layout';
 import { getSortedPostsData } from '../lib/posts';
 import HomeIntro from '../components/home_compon/HomeIntro';
 import TagBox from '../components/home_compon/TagBox';
 import HomeSplash from '../components/home_compon/HomeSplash';
-export default function Home({ allPostsData }) {
+import Pagination from '../components/home_compon/Pagination';
+
+const POST_PER_PAGE = 6;
+
+export function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  const initialPosts = allPostsData.slice(0, POST_PER_PAGE);
+  const paginations = {
+    currentPage: 1,
+    totalPages: Math.ceil(allPostsData.length / POST_PER_PAGE),
+  };
+  return {
+    // props: {
+    //   allPostsData,
+    // },
+    props: {
+      allPostsData,
+      initialPosts,
+      paginations,
+    },
+  };
+}
+
+export default function Home({ initialPosts, open, paginations }) {
   return (
     <>
-      <HomeSplash />
-      <Layout home>
-        <section>
-          <HomeIntro />
-        </section>
-        <section>
-          <Typography variant='h4' pt='24px' pb='32px'>
-            Posts
-          </Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
-              <TagBox />
-            </Grid>
-            <Grid container item xs={12} md={8} spacing={2}>
-              {allPostsData.map(({ id, title, date, tags, images }) => (
-                <Grid item xs={12} sm={6} key={id}>
-                  <Card variant='outlined'>
-                    {/* <CardMedia image={`${images}`} title='123' /> */}
-                    <Image
+      {open ? (
+        <HomeSplash />
+      ) : (
+        <Layout home>
+          <section>
+            <HomeIntro />
+          </section>
+          <section>
+            <Typography variant='h4' pt='24px' pb='32px'>
+              Posts
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={3}>
+                <TagBox />
+              </Grid>
+              <Grid container item xs={12} md={9} spacing={2}>
+                {initialPosts.map(({ id, title, date, tags, images }) => (
+                  <Grid item xs={12} sm={6} key={id}>
+                    <Card variant='outlined' sx={{ height: '100%' }}>
+                      <CardMedia image={`${images}`} title='123' />
+                      {/* <Image
                       src={`${images}`}
                       alt='1223'
                       width={150}
                       height={150}
-                    />
-                    <CardContent>
-                      <Link href={`/posts/${id}`} passHref legacyBehavior>
-                        <Typography
-                          gutterBottom
-                          variant='h5'
+                    /> */}
+                      <CardContent>
+                        <Link href={`/posts/${id}`} passHref legacyBehavior>
+                          <Typography
+                            gutterBottom
+                            variant='h5'
+                            sx={{
+                              '&:hover': {
+                                color: 'primary.main',
+                                cursor: 'pointer',
+                              },
+                            }}
+                          >
+                            {title}
+                          </Typography>
+                        </Link>
+                        <Typography gutterBottom variant='h6'>
+                          {date}
+                        </Typography>
+                        <Box
                           sx={{
-                            '&:hover': {
-                              color: 'primary.main',
-                              cursor: 'pointer',
-                            },
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            gap: '10px',
                           }}
                         >
-                          {title}
-                        </Typography>
-                      </Link>
-                      <Typography gutterBottom variant='h6'>
-                        {date}
-                      </Typography>
-                      <Box
-                        sx={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}
-                      >
-                        {tags.map((tag, idx) => (
-                          <Link
-                            key={idx}
-                            href={`/tags/${tag}`}
-                            passHref
-                            legacyBehavior
+                          {tags.map((tag, idx) => (
+                            <Link
+                              key={idx}
+                              href={`/tags/${tag}`}
+                              passHref
+                              legacyBehavior
+                            >
+                              <Chip
+                                label={`${tag}`}
+                                variant='filled'
+                                clickable
+                              />
+                            </Link>
+                          ))}
+                        </Box>
+                      </CardContent>
+                      <CardActions>
+                        <Link href={`/posts/${id}`} passHref legacyBehavior>
+                          <Button
+                            size='small'
+                            endIcon={<ArrowForwardIosIcon />}
+                            sx={{ fontWeight: 700 }}
                           >
-                            <Chip label={`${tag}`} variant='filled' clickable />
-                          </Link>
-                        ))}
-                      </Box>
-                    </CardContent>
-                    <CardActions>
-                      <Link href={`/posts/${id}`} passHref legacyBehavior>
-                        <Button size='small' sx={{ fontWeight: 700 }}>
-                          全文
-                        </Button>
-                      </Link>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
+                            全文
+                          </Button>
+                        </Link>
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
-          </Grid>
-        </section>
-      </Layout>
+            {paginations.totalPages > 1 && (
+              <Pagination paginations={paginations} />
+            )}
+          </section>
+        </Layout>
+      )}
     </>
   );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
 }
